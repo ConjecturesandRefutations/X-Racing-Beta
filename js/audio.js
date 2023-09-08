c777ad0cb42c674c2386a0d5ef383a44d425d9dd
Alfie Collins <alfiecollins44@gmail.com>
2023-09-08 21:36:43 +0100

    removing the opening audio bug

--- js/audio.js
+++ js/audio.js
@@ -1,61 +1,52 @@
 let crash = new Audio('./audio/crash.mp3');
-
 let drive = new Audio('./audio/drive.mp3');
-
 let turn = new Audio('./audio/turn.mp3');
-
 let congrats = new Audio('./audio/congrats.mp3');
-
 let opening = new Audio('./audio/opening.mp3');
-
 let closing = new Audio('./audio/closing.mp3');
-
 let yummy = new Audio('./audio/yummy.mp3');
-
 let skull = new Audio('./audio/skull.mp3');
+let openingAudioPlaying = false;
 
-document.addEventListener("DOMContentLoaded", function () {
-  let opening = new Audio('./audio/opening.mp3');
-  let openingAudioPlaying = false;
+// Function to start playing the opening audio
+function playOpeningAudio() {
+  opening.play()
+    .then(() => {
+      openingAudioPlaying = true;
+    })
+    .catch((error) => {
+      console.error('Error playing opening audio:', error);
+      openingAudioPlaying = false;
+    });
+}
 
-  // Function to start playing the opening audio
-  function playOpeningAudio() {
-    opening.play()
-      .then(() => {
-        openingAudioPlaying = true;
-      })
-      .catch((error) => {
-        console.error('Error playing opening audio:', error);
-        openingAudioPlaying = false;
-      });
-  }
+// Function to pause the opening audio
+function pauseOpeningAudio() {
+  opening.pause();
+  openingAudioPlaying = false;
+  volumeIcon.classList.remove('fa', 'fa-volume-up');
+  volumeIcon.classList.add('fa', 'fa-volume-mute');
+}
 
-  // Function to pause the opening audio
-  function pauseOpeningAudio() {
-    opening.pause();
-    openingAudioPlaying = false;
-  }
+const openingAudio = document.querySelector('.play-music');
+const volumeIcon = document.getElementById('volume-icon');
 
-  const openingAudio = document.querySelector('.play-music');
-  const volumeIcon = document.getElementById('volume-icon');
-
-  // Add a click event listener to the opening audio element
-  openingAudio.addEventListener('click', () => {
-    if (openingAudioPlaying) {
-      pauseOpeningAudio();
-      volumeIcon.classList.remove('fa', 'fa-volume-up');
-      volumeIcon.classList.add('fa', 'fa-volume-mute');
-    } else {
-      playOpeningAudio();
-      volumeIcon.classList.remove('fa', 'fa-volume-mute');
-      volumeIcon.classList.add('fa', 'fa-volume-up');
-    }
-  });
-
-  // Event listener for the "ended" event of the opening audio
-  opening.addEventListener('ended', () => {
-    openingAudioPlaying = false;
+// Add a click event listener to the opening audio element
+openingAudio.addEventListener('click', () => {
+  if (openingAudioPlaying) {
+    pauseOpeningAudio();
     volumeIcon.classList.remove('fa', 'fa-volume-up');
     volumeIcon.classList.add('fa', 'fa-volume-mute');
-  });
+  } else {
+    playOpeningAudio();
+    volumeIcon.classList.remove('fa', 'fa-volume-mute');
+    volumeIcon.classList.add('fa', 'fa-volume-up');
+  }
+});
+
+// Event listener for the "ended" event of the opening audio
+opening.addEventListener('ended', () => {
+  openingAudioPlaying = false;
+  volumeIcon.classList.remove('fa', 'fa-volume-up');
+  volumeIcon.classList.add('fa', 'fa-volume-mute');
 });
